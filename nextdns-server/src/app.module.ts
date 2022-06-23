50d8e284b2517833be8f5e6bc501b570809999ab
Dave Thomas <dave@monstergfx.com>
2022-06-23 14:51:33 -0700

    chore: register proxy controller

--- nextdns-server/src/app.module.ts
+++ nextdns-server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProxyController } from './proxy.controller';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, ProxyController],
   providers: [AppService],
 })
 export class AppModule {}
