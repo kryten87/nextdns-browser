ff0d36913f1921007f477c981bf7faaa13fd8536
Dave Thomas <dave@monstergfx.com>
2022-06-26 08:25:52 -0700

    chore: register queue service

--- nextdns-server/src/app.module.ts
+++ nextdns-server/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxyController } from './proxy.controller';
 import { ConfigModule } from '@nestjs/config';
+import { QueueService } from './queue.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController, ProxyController],
-  providers: [AppService],
+  providers: [AppService, QueueService],
 })
 export class AppModule {}
