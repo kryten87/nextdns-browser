1bea9d447047187beded3338ae0e8e4f85da0ef0
Dave Thomas <dave@monstergfx.com>
2022-06-26 08:27:22 -0700

    chore: add streaming, queue to controller

--- nextdns-server/src/app.controller.ts
+++ nextdns-server/src/app.controller.ts
@@ -1,12 +1,45 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { QueueService } from './queue.service';
+import * as EventSource from 'eventsource';
+import { ConfigService } from '@nestjs/config';
+import { writeFileSync } from 'fs';
 
+const values = [];
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly apiKey: string;
+  private readonly baseUrl: string;
+  private eventSource: any;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(
+    private readonly appService: AppService,
+    private readonly queueService: QueueService,
+    private readonly configService: ConfigService,
+  ) {
+    this.apiKey = this.configService.get<string>('NEXTDNS_API_KEY');
+    this.baseUrl = this.configService.get<string>('NEXTDNS_BASE_URL');
+    this.eventSource = new EventSource(
+      `${this.baseUrl}profiles/963ede/logs/stream`,
+      {
+        headers: { 'X-Api-Key': this.apiKey },
+      },
+    );
+    this.eventSource.onmessage = this.streamHandler;
+
+    this.queueService.onLogQueueMessage(this.queueHandler);
   }
+
+  streamHandler = (message) => {
+    this.queueService.sendToLogQueue(message);
+  };
+
+  queueHandler = async (message) => {
+    const data = JSON.parse(JSON.parse(message)?.data || '{}');
+    console.log('..... queue handler', data);
+    values.push(data);
+    if (values.length === 10) {
+      writeFileSync('./exampledata.json', JSON.stringify(values, null, 2));
+    }
+  };
 }
