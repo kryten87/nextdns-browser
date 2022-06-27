3a564000da782a6385b9b5bdf5c41e5ab4156522
Dave Thomas <dave@monstergfx.com>
2022-06-27 14:06:15 -0700

    chore: add database setup to database service

--- nextdns-server/src/database.service.ts
+++ nextdns-server/src/database.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import knex from 'knex';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService {
+  private type: string;
+  private connection: any;
+
+  constructor(private readonly configService: ConfigService) {
+    const client = this.configService.get<string>('DATABASE_CLIENT');
+    const params = {
+      client,
+      ...(client === 'sqlite3' && {
+        filename: this.configService.get<string>('SQLITE_FILE'),
+        useNullAsDefault: true,
+      }),
+      ...(client === 'mysql' && {
+        host: this.configService.get<string>('MARIADB_HOST'),
+        port: this.configService.get<string>('MARIADB_PORT'),
+        database: this.configService.get<string>('MARIADB_DATABASE'),
+        user: this.configService.get<string>('MARIADB_USER'),
+        password: this.configService.get<string>('MARIADB_PASSWORD'),
+      }),
+    };
+    this.connection = knex(params);
+  }
+
+
+}
