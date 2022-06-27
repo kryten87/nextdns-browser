83b447989664bf85ff0c6897a1b216de466c8f6f
Dave Thomas <dave@monstergfx.com>
2022-06-27 14:06:05 -0700

    chore: add prelim specs

--- nextdns-server/src/database.service.spec.ts
+++ nextdns-server/src/database.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DatabaseService } from './database.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('DatabaseService', () => {
   let service: DatabaseService;
 
+  const mockConfigService = {
+    get: (key: string): string => {
+      if (key === 'DATABASE_CLIENT') {
+        return 'sqlite3';
+      } else if (key === 'SQLITE_FILE') {
+        return ':memory:';
+      }
+      throw new Error(`unexpected config key "${key}"`);
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DatabaseService],
+      providers: [
+        DatabaseService,
+        { provide: ConfigService, useValue: mockConfigService },
+      ],
     }).compile();
 
     service = module.get<DatabaseService>(DatabaseService);
@@ -15,4 +30,21 @@ describe('DatabaseService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('insertProfile', () => {
+    it.todo('should correctly insert a profile');
+  });
+
+  describe('insertDevice', () => {
+    it.todo('should correctly insert a device');
+  });
+
+  describe('insertEvent', () => {
+    it.todo('should correctly insert an event');
+  });
+
+  describe('getDevices', () => {
+    it.todo('should get the expected results');
+  });
+
 });
