0bb426c1d6c93acf2e31abd91ce267d0cb6682b9
Dave Thomas <dave@monstergfx.com>
2022-06-29 07:36:42 -0700

    chore: implement startup service

--- nextdns-server/src/services/start-up.service.ts
+++ nextdns-server/src/services/start-up.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DatabaseService } from './database.service';
+import { NextDnsApiService } from './next-dns-api.service';
 
 @Injectable()
-export class StartUpService {}
+export class StartUpService implements OnModuleInit {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly databaseService: DatabaseService,
+    private readonly nextDnsApiService: NextDnsApiService,
+  ) {}
+
+  async onModuleInit() {
+    // perform any necessary migrations
+    await this.databaseService.migrateLatest();
+    // perform the app startup tasks
+    await Promise.all([this.updateProfiles()]);
+  }
+
+  async updateProfiles(): Promise<void> {
+    const profiles = await this.nextDnsApiService.getProfiles();
+    await Promise.all(
+      profiles.map((profile) => this.databaseService.insertProfile(profile)),
+    );
+  }
+}
