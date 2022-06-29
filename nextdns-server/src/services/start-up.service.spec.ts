7e096b5a09c8319d2cc6a20b377441874b754ef6
Dave Thomas <dave@monstergfx.com>
2022-06-29 07:35:54 -0700

    test: add spec for get profile on startup

--- nextdns-server/src/services/start-up.service.spec.ts
+++ nextdns-server/src/services/start-up.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StartUpService } from './start-up.service';
+import { ConfigService } from '@nestjs/config';
+import { DatabaseService } from './database.service';
+import { NextDnsApiService } from './next-dns-api.service';
 
 describe('StartUpService', () => {
   let service: StartUpService;
 
+  const profiles = ['profile 1', 'profile 2', 'profile 3'];
+
+  const mockApi = {
+    getProfiles: jest.fn().mockResolvedValue(profiles),
+  };
+  const mockDb = {
+    insertProfile: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StartUpService],
+      providers: [
+        StartUpService,
+        ConfigService,
+        { provide: DatabaseService, useValue: mockDb },
+        { provide: NextDnsApiService, useValue: mockApi },
+      ],
     }).compile();
 
     service = module.get<StartUpService>(StartUpService);
@@ -14,5 +31,16 @@ describe('StartUpService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('updateProfiles', () => {
+    it('should get the profiles from the API and save them in the database', async () => {
+      await service.updateProfiles();
+      expect(mockApi.getProfiles.mock.calls.length).toBe(1);
+      expect(mockDb.insertProfile.mock.calls.length).toBe(profiles.length);
+      expect(mockDb.insertProfile.mock.calls).toEqual(
+        profiles.map((profile) => [profile]),
+      );
+    });
   });
 });
