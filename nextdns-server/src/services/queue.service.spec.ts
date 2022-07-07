462645ca28534ce85215dbbe44361d7f07997ea1
Dave Thomas <dave@monstergfx.com>
2022-07-07 13:53:12 -0700

    test: add prelim testing plan

--- nextdns-server/src/services/queue.service.spec.ts
+++ nextdns-server/src/services/queue.service.spec.ts
@@ -16,4 +16,28 @@ describe('QueueService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('onModuleInit', () => {
+    it.todo('should connect to the messaging service');
+
+    it.todo('should create a channel');
+
+    it.todo('should check to ensure the queue is set up');
+  });
+
+  describe('onModuleDestroy', () => {
+    it.todo('should close the connection');
+  });
+
+  describe('sendToLogQueue', () => {
+    it.todo('should send to message to the correct queue');
+  });
+
+  describe('onLogQueueMessage', () => {
+    it.todo('should consume the message');
+
+    it.todo('should call the callback function');
+
+    it.todo('should acknowledge the message');
+  });
 });
