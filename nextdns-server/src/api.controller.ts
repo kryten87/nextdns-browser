f3d505d55c222e88db8b9b02eec0c33bc17c420e
Dave Thomas <dave@monstergfx.com>
2022-07-28 07:31:40 -0700

    chore: linting

--- nextdns-server/src/api.controller.ts
+++ nextdns-server/src/api.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DatabaseService } from './services/database.service';
 
 @Controller('api')
