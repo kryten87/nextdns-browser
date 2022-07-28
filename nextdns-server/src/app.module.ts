004823f7e12522638928a152d4dcf1ad6842f851
Dave Thomas <dave@monstergfx.com>
2022-07-28 07:28:04 -0700

    chore: remove reference to proxy

--- nextdns-server/src/app.module.ts
+++ nextdns-server/src/app.module.ts
@@ -3,13 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './services/database.service';
 import { Module } from '@nestjs/common';
 import { NextDnsApiService } from './services/next-dns-api.service';
-import { ProxyController } from './proxy.controller';
 import { QueueService } from './services/queue.service';
 import { StartUpService } from './services/start-up.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, ProxyController],
+  controllers: [AppController],
   providers: [QueueService, DatabaseService, NextDnsApiService, StartUpService],
 })
 export class AppModule {}
