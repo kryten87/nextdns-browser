94d83b34343e467d89649b24f2d1d9f9aca6f6af
Dave Thomas <dave@monstergfx.com>
2022-07-28 10:24:51 -0700

    chore: enable nestjs-knex package

--- nextdns-server/src/app.module.ts
+++ nextdns-server/src/app.module.ts
@@ -1,14 +1,48 @@
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseService } from './services/database.service';
 import { Module } from '@nestjs/common';
 import { NextDnsApiService } from './services/next-dns-api.service';
 import { QueueService } from './services/queue.service';
 import { StartUpService } from './services/start-up.service';
 import { ApiController } from './api.controller';
+import { KnexModule } from 'nestjs-knex';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+
+    KnexModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const client = configService.get<string>('DATABASE_CLIENT');
+        return {
+          config: {
+            client,
+
+            ...(client === 'sqlite3' && {
+              useNullAsDefault: true,
+              connection: {
+                filename: configService.get<string>('SQLITE_FILE'),
+              },
+            }),
+
+            ...(client === 'mysql2' && {
+              connection: {
+                host: configService.get<string>('MARIADB_HOST'),
+                port: configService.get<string>('MARIADB_PORT'),
+                database: configService.get<string>('MARIADB_DATABASE'),
+                user: configService.get<string>('MARIADB_USER'),
+                password: configService.get<string>('MARIADB_PASSWORD'),
+              },
+            }),
+
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController, ApiController],
   providers: [QueueService, DatabaseService, NextDnsApiService, StartUpService],
 })
