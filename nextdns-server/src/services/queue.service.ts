4ed551a0535081410f22ec00c1fbd5cceb655e36
Dave Thomas <dave@monstergfx.com>
2022-07-28 10:28:56 -0700

    chore: rewrite polling

--- nextdns-server/src/services/queue.service.ts
+++ nextdns-server/src/services/queue.service.ts
@@ -17,6 +17,7 @@ export class QueueService implements OnModuleInit, OnModuleDestroy {
   private incomingLogQueue: string;
   private amqpConnection: amqp.Connection;
   private amqpChannel: any;
+  private messageHandler: any;
 
   constructor(
     private readonly config: ConfigService,
@@ -33,6 +34,9 @@ export class QueueService implements OnModuleInit, OnModuleDestroy {
     this.amqpConnection = await this.amqpLib.connect(this.rabbitUrl);
     this.amqpChannel = await this.amqpConnection.createChannel();
     await this.amqpChannel.assertQueue(this.incomingLogQueue);
+    this.amqpChannel.prefetch(10);
+
+    this.poll();
   }
 
   async onModuleDestroy() {
@@ -50,14 +54,22 @@ export class QueueService implements OnModuleInit, OnModuleDestroy {
   }
 
   async onLogQueueMessage(handler: any) {
-    try {
-      this.amqpChannel.consume(this.incomingLogQueue, async (message: any) => {
-        await handler(JSON.parse(message.content.toString()));
-        this.amqpChannel.ack(message);
-      });
-    } catch (err) {
-      await pause(10);
-      return this.onLogQueueMessage(handler);
+    this.messageHandler = handler;
+  }
+
+  private async poll() {
+    if (this.amqpChannel?.consume && this.messageHandler) {
+      try {
+        this.amqpChannel.consume(this.incomingLogQueue, async (message: any) => {
+          await this.messageHandler(JSON.parse(message.content.toString()));
+          this.amqpChannel.ack(message);
+        }, { noAck: false });
+      } catch (err) {
+        console.error('error on message handling', err);
+      }
     }
+
+    await pause(100);
+    return this.poll();
   }
 }
