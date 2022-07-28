3146fa8b830a36bc526f73a1488aa2ec34a4a681
Dave Thomas <dave@monstergfx.com>
2022-07-28 10:26:07 -0700

    chore: implement nestjs-knex package

--- nextdns-server/src/services/database.service.ts
+++ nextdns-server/src/services/database.service.ts
@@ -2,7 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { MigrationSource } from '../libs/migration-source';
 import * as crypto from 'crypto';
-import knex from 'knex';
+import { InjectKnex, Knex } from 'nestjs-knex';
 
 export interface Profile {
   id: string;
@@ -55,59 +55,28 @@ export interface EventModel {
 }
 
 @Injectable()
-export class DatabaseService implements OnModuleDestroy {
-  private type: string;
-  // @TODO make this private
-  connection: any;
-
-  constructor(private readonly configService: ConfigService) {
-    const client = this.configService.get<string>('DATABASE_CLIENT');
-    const params = {
-      client,
-      ...(client === 'sqlite3' && {
-        useNullAsDefault: true,
-        connection: {
-          filename: this.configService.get<string>('SQLITE_FILE'),
-        },
-      }),
-      ...(client === 'mysql2' && {
-        connection: {
-          host: this.configService.get<string>('MARIADB_HOST'),
-          port: this.configService.get<string>('MARIADB_PORT'),
-          database: this.configService.get<string>('MARIADB_DATABASE'),
-          user: this.configService.get<string>('MARIADB_USER'),
-          password: this.configService.get<string>('MARIADB_PASSWORD'),
-        },
-      }),
-    };
-    this.connection = knex(params);
-  }
-
-  async onModuleDestroy() {
-    if (this.connection) {
-      await this.connection.destroy();
-    }
-  }
+export class DatabaseService  /* implements OnModuleDestroy */ {
+  constructor(@InjectKnex() private readonly knex: Knex) {}
 
   async migrateLatest(): Promise<void> {
-    await this.connection.migrate.latest({
+    await this.knex.migrate.latest({
       migrationSource: new MigrationSource(),
     });
   }
 
   async insertProfile(profile: Profile): Promise<string> {
-    await this.connection('profiles').insert(profile).onConflict().ignore();
+    await this.knex.table('profiles').insert(profile).onConflict().ignore();
     return profile.id;
   }
 
   async setLastEventId(profileId: string, lastEventId: string): Promise<void> {
-    await this.connection('profiles')
+    await this.knex.table('profiles')
       .update({ lastEventId })
       .where('id', '=', profileId);
   }
 
   async insertDevice(device: Device): Promise<string> {
-    await this.connection('devices').insert(device).onConflict().ignore();
+    await this.knex.table('devices').insert(device).onConflict().ignore();
     return device.id;
   }
 
@@ -121,7 +90,7 @@ export class DatabaseService implements OnModuleDestroy {
       )
       .digest('hex');
     const timestamp = new Date(event.timestamp).valueOf() / 1000;
-    await this.connection('events')
+    await this.knex.table('events')
       .insert({
         id,
         timestamp,
@@ -146,10 +115,10 @@ export class DatabaseService implements OnModuleDestroy {
   }
 
   async getDevices(): Promise<Device[]> {
-    return this.connection('devices').select();
+    return this.knex.table('devices').select();
   }
 
   async getProfiles(): Promise<Profile[]> {
-    return this.connection('profiles').select();
+    return this.knex.table('profiles').select();
   }
 }
