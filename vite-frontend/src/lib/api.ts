222aa8c04edad4fc7c04339b191e96a68682544b
Dave Thomas <dave@monstergfx.com>
2022-08-09 13:55:43 -0700

    chore: add fcn to clean up query

--- vite-frontend/src/lib/api.ts
+++ vite-frontend/src/lib/api.ts
@@ -15,13 +15,22 @@ export const getProfiles = async (): Promise<Profile[]> => {
   return result.json();
 };
 
+const stripUndefinedProperties = (obj: { [key: string]: string | number | undefined | null }): { [key: string]: string } => {
+  const result = { ...obj };
+  Object.keys(result).forEach((key) => {
+    if (!result[key] && result[key] !== 0) {
+      delete result[key];
+    }
+  });
+  return result as { [key: string]: string };
+};
+
 export const getEvents = async (
   parameters: SearchParameters,
 ): Promise<SearchResponse> => {
   // @TODO add pagination
-  const url = `${baseUrl}/events?${new URLSearchParams(
-    parameters as unknown as { [key: string]: string },
-  ).toString()}`;
+  const params: { [key: string]: string } = stripUndefinedProperties({ ...parameters });
+  const url = `${baseUrl}/events?${new URLSearchParams(params).toString()}`;
   const result = await fetch(url);
   const data = await result.json();
   return data;
