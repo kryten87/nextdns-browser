549f1caa1c6baa75a5eb570a88d580dbda749b01
Dave Thomas <dave@monstergfx.com>
2022-08-09 11:49:40 -0700

    chore: add env variable to disable messages

--- nestjs-server/src/app.controller.ts
+++ nestjs-server/src/app.controller.ts
@@ -2,29 +2,40 @@ import { Controller, OnModuleInit } from '@nestjs/common';
 import { DatabaseService } from './services/database.service';
 import { NextDnsApiService } from './services/next-dns-api.service';
 import { QueueService } from './services/queue.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController implements OnModuleInit {
   private eventSources: any[] = [];
 
   constructor(
+    private readonly configService: ConfigService,
     private readonly queueService: QueueService,
     private readonly db: DatabaseService,
     private readonly api: NextDnsApiService,
   ) {
-    this.queueService.onLogQueueMessage(this.queueHandler);
+    if (!this.configService.get<boolean>('DISABLE_INCOMING_MESSAGES')) {
+      this.queueService.onLogQueueMessage(this.queueHandler);
+    } else {
+      // @TODO add NestJS compliant logging
+      console.log('queued message handling disabled');
+    }
   }
 
   async onModuleInit() {
-    const profiles = await this.db.getProfiles();
-    profiles.forEach((profile) => {
-      this.eventSources.push(
-        this.api.initializeEventSource(
-          profile,
-          this.createStreamHandler(profile.id),
-        ),
-      );
-    });
+    if (!this.configService.get<boolean>('DISABLE_INCOMING_MESSAGES')) {
+      const profiles = await this.db.getProfiles();
+      profiles.forEach((profile) => {
+        this.eventSources.push(
+          this.api.initializeEventSource(
+            profile,
+            this.createStreamHandler(profile.id),
+          ),
+        );
+      });
+    } else {
+      console.log('incoming messages functionality disabled');
+    }
   }
 
   createStreamHandler = (profile: string) => {
